ea228889f4c5a1861140125532fa3d247ede675d
Kendrick Parks <bordersmbk02@aol.com>
2021-02-02 00:23:57 -0800

    more changes for initial app

--- server/index.js
+++ server/index.js
@@ -1,14 +1,44 @@
 require('dotenv').config({ path: __dirname + '/../.env' })
+const http = require('http')
 const path = require('path')
+const cors = require('cors')
 const express = require('express')
 const app = express()
+const { errorHandler } = require('./helpers/helpers')
+const { mailer } = require('./helpers/mailer')
+const PORT = process.env.PORT || 8080;
+
+app.use(cors({
+    allowedHeaders: [
+        'Refresh-Token', 'Content-Type', 'Authorization'
+    ]
+}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../app')));
+app.use(errorHandler)
+
 
 app.get('/api/health', (req, res) => {
     res.status(200).send('Server Healthy')
 })
 
-app.use(express.static(path.join(__dirname, '../app')));
+const server = http.createServer(app);
+server.listen(PORT, async () => console.log('Server listening on:' + PORT));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening port 8000`)
+process.on('uncaughtException', (err) => {
+    console.error('uncaughtException', err)
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (err) => {
+    console.error('unhandledRejection', err)
+    process.exit(1)
+})
+
+process.on('beforeExit', (err) => {
+    mailer.close()
+    server.close()
+    console.log('beforeExit', err)
+    process.exit(1)
 })
