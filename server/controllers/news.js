93d5ee448c4b77ffef50b5265772ecc8384acc25
Kendrick Parks <bordersmbk02@aol.com>
2021-02-15 11:31:14 -0800

    create news

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -1,10 +1,43 @@
 const { Router } = require('express')
-const { asyncHandler } = require('../helpers/helpers')
+const { asyncHandler, validToken, permissionCheck, ROLES } = require('../helpers/helpers')
+const _ = require('lodash')
+const News = require('../models/news')
+const NewsService = require('../services/news')
+const ProgramService = require('../services/program')
 
 module.exports = class NewsController {
     constructor() {
         this.router = new Router()
+        this.newsService = new NewsService()
+        this.programService = new ProgramService()
 
+        this.router.post('/', validToken, permissionCheck(ROLES.OWNER, ROLES.INSTRUCTOR), asyncHandler(this.createNews.bind(this)))
+    }
 
+    async createNews(req, res, next) {
+        if (!req.query.programId || _.isEmpty(req.query.programId)) {
+            return next({ code: 400, message: 'Unable to create news. Missing query param programId' })
+        }
+
+        let existingProgram = await this.programService.getProgramById(req.query.programId)
+
+        if (!existingProgram) {
+            return next({ code: 400, message: `No program with the programId ${req.query.programId} exists.` })
+        }
+
+        let newNews = new News(Object.assign({}, req.body, {
+            programId: req.query.programId,
+            createdBy: req.user.id,
+            updatedBy: req.user.id
+        }))
+
+        let errors = News.validate(newNews)
+        if (errors) {
+            return next({ code: 400, message: errors.join('. ') })
+        }
+
+        let news = (await this.newsService.createNews(newNews)).viewable
+
+        res.status(201).json({ news })
     }
 }
---
+++ server/services/news.js
@@ -0,0 +1,62 @@
+const { database } = require('../db/database')
+const _ = require('lodash')
+const News = require('../models/news')
+
+module.exports = class NewsService {
+    constructor() { }
+
+    async getNewsById(id) {
+        const sql = 'SELECT * FROM News WHERE id=?;'
+        const values = [id]
+
+        try {
+            let results = await database.query(sql, values) || []
+
+            if (results && results.length > 0) {
+                return new News(results[0])
+            }
+
+            return
+        } catch (err) {
+            console.log(err)
+            return Promise.reject({ code: 500, message: 'Unable to get news.' })
+        }
+    }
+
+    async getNewsByProgramId(programId) {
+        const sql = 'SELECT * FROM News WHERE programId=?;'
+        const values = [programId]
+
+        try {
+            let results = await database.query(sql, values) || []
+
+            return results.map(news => new News(news))
+        } catch (err) {
+            console.log(err)
+            return Promise.reject({ code: 500, message: 'Unable to get news.' })
+        }
+    }
+
+    async createNews(data) {
+        const sql = 'INSERT INTO News SET ?;'
+        const values = [_.pickBy(data, _.identity)] // _.pickBy ... _.identity removed null and undefined from object
+
+        try {
+            await database.query(sql, values)
+            let news = await this.getNewsById(data.id)
+
+            if (!news) {
+                return Promise.reject({ code: 500, message: 'Unable to get News after Creation.' })
+            }
+
+            return news
+        } catch (err) {
+            console.log(err)
+            if (typeof err === 'object') {
+                return Promise.reject(err)
+            }
+
+            return Promise.reject({ code: 500, message: 'Unable to create News.' })
+        }
+    }
+}
