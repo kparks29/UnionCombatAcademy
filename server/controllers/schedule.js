8f812f7a7a5d9a491f33f0832be2dbe7c46f675c
Kendrick Parks <bordersmbk02@aol.com>
2021-02-15 12:16:50 -0800

    crud on schedules

--- server/controllers/schedule.js
+++ server/controllers/schedule.js
@@ -1,10 +1,120 @@
 const { Router } = require('express')
-const { asyncHandler } = require('../helpers/helpers')
+const { asyncHandler, validToken, permissionCheck, ROLES } = require('../helpers/helpers')
+const _ = require('lodash')
+const Schedule = require('../models/schedule')
+const ScheduleService = require('../services/schedule')
+const ProgramService = require('../services/program')
 
 module.exports = class ScheduleController {
     constructor() {
         this.router = new Router()
+        this.scheduleService = new ScheduleService()
+        this.programService = new ProgramService()
 
+        this.router.get('/', validToken, permissionCheck(ROLES.OWNER, ROLES.INSTRUCTOR, ROLES.STUDENT), asyncHandler(this.getSchedules.bind(this)))
+        this.router.post('/', validToken, permissionCheck(ROLES.OWNER, ROLES.INSTRUCTOR), asyncHandler(this.createSchedule.bind(this)))
+        this.router.put('/:scheduleId', validToken, permissionCheck(ROLES.OWNER, ROLES.INSTRUCTOR), asyncHandler(this.updateSchedule.bind(this)))
+        this.router.delete('/:scheduleId', validToken, permissionCheck(ROLES.OWNER, ROLES.INSTRUCTOR), asyncHandler(this.deleteSchedule.bind(this)))
+    }
 
+    async getSchedules(req, res, next) {
+        if (!req.query.programId || _.isEmpty(req.query.programId)) {
+            return next({ code: 400, message: 'Unable to get schedule. Missing query param programId' })
+        }
+
+        let schedules = (await this.scheduleService.getSchedulesByProgramId(req.query.programId)).map(schedule => schedule.viewable)
+
+        res.status(200).json({ schedules })
+    }
+
+    async createSchedule(req, res, next) {
+        if (!req.query.programId || _.isEmpty(req.query.programId)) {
+            return next({ code: 400, message: 'Unable to create schedule. Missing query param programId' })
+        }
+
+        let existingProgram = await this.programService.getProgramById(req.query.programId)
+
+        if (!existingProgram) {
+            return next({ code: 400, message: `No program with the programId ${req.query.programId} exists.` })
+        }
+
+        let newSchedule = new Schedule(Object.assign({}, req.body, {
+            programId: req.query.programId,
+            createdBy: req.user.id,
+            updatedBy: req.user.id
+        }))
+
+        let errors = Schedule.validate(newSchedule)
+        if (errors) {
+            return next({ code: 400, message: errors.join('. ') })
+        }
+
+        let schedule = (await this.scheduleService.createSchedule(newSchedule)).viewable
+
+        res.status(201).json({ schedule })
+    }
+
+    async updateSchedule(req, res, next) {
+        if (!req.params.scheduleId || _.isEmpty(req.params.scheduleId)) {
+            return next({ code: 400, message: 'Unable to update schedule. Missing scheduleId' })
+        }
+
+        let existingSchedule = await this.scheduleService.getScheduleById(req.params.scheduleId)
+
+        if (!existingSchedule) {
+            return next({ code: 400, message: `No schedule with the scheduleId ${req.params.scheduleId} exists.` })
+        }
+
+        if (!req.query.programId || _.isEmpty(req.query.programId)) {
+            return next({ code: 400, message: 'Unable to update schedule. Missing programId' })
+        }
+
+        let existingProgram = await this.programService.getProgramById(req.query.programId)
+
+        if (!existingProgram) {
+            return next({ code: 400, message: `No program with the programId ${req.query.programId} exists.` })
+        }
+
+        // _.pickBy ... _.identity removed null and undefined from object
+        let updatedSchedule = new Schedule(_.pickBy(
+            Object.assign(
+                {},
+                existingSchedule,
+                _.omit(req.body, ['id', 'programId', 'createdAt', 'updatedAt', 'createdBy', 'updatedBy']),
+                { updatedBy: req.user.id }
+            ), _.identity)
+        )
+        updatedSchedule = _.omit(updatedSchedule, ['createdAt', 'updatedAt'])
+
+        let errors = Schedule.validate(updatedSchedule)
+        if (errors) {
+            return next({ code: 400, message: errors.join('. ') })
+        }
+
+        let schedule = (await this.scheduleService.updateSchedule(req.params.scheduleId, updatedSchedule)).viewable
+
+        res.status(200).json({ schedule })
+    }
+
+    async deleteSchedule(req, res, next) {
+        if (!req.params.scheduleId || _.isEmpty(req.params.scheduleId)) {
+            return next({ code: 400, message: 'Unable to delete schedule. Missing scheduleId' })
+        }
+
+        if (!await this.scheduleService.getScheduleById(req.params.scheduleId)) {
+            return next({ code: 400, message: `Unable to delete schedule. Schedule does not exist.` })
+        }
+
+        if (!req.query.programId || _.isEmpty(req.query.programId)) {
+            return next({ code: 400, message: 'Unable to delete schedule. Missing programId' })
+        }
+
+        if (!await this.programService.getProgramById(req.query.programId)) {
+            return next({ code: 400, message: `No program with the programId ${req.query.programId} exists.` })
+        }
+
+        await this.scheduleService.deleteSchedule(req.params.scheduleId)
+
+        res.status(204).json({ message: 'Successfully deleted schedule' })
     }
 }
---
+++ server/services/schedule.js
@@ -0,0 +1,97 @@
+const { database } = require('../db/database')
+const _ = require('lodash')
+const Schedule = require('../models/schedule')
+
+module.exports = class ScheduleService {
+    constructor() { }
+
+    async getScheduleById(id) {
+        const sql = 'SELECT * FROM Schedules WHERE id=?;'
+        const values = [id]
+
+        try {
+            let results = await database.query(sql, values) || []
+
+            if (results && results.length > 0) {
+                return new Schedule(results[0])
+            }
+
+            return
+        } catch (err) {
+            console.log(err)
+            return Promise.reject({ code: 500, message: 'Unable to get schedule.' })
+        }
+    }
+
+    async getSchedulesByProgramId(programId) {
+        const sql = 'SELECT * FROM Schedules WHERE programId=?;'
+        const values = [programId]
+
+        try {
+            let results = await database.query(sql, values) || []
+
+            return results.map(schedule => new Schedule(schedule))
+        } catch (err) {
+            console.log(err)
+            return Promise.reject({ code: 500, message: 'Unable to get schedule.' })
+        }
+    }
+
+    async createSchedule(data) {
+        const sql = 'INSERT INTO Schedules SET ?;'
+        const values = [_.pickBy(data, _.identity)] // _.pickBy ... _.identity removed null and undefined from object
+
+        try {
+            await database.query(sql, values)
+            let schedule = await this.getScheduleById(data.id)
+
+            if (!schedule) {
+                return Promise.reject({ code: 500, message: 'Unable to get schedule after Creation.' })
+            }
+
+            return schedule
+        } catch (err) {
+            console.log(err)
+            if (typeof err === 'object') {
+                return Promise.reject(err)
+            }
+
+            return Promise.reject({ code: 500, message: 'Unable to create schedule.' })
+        }
+    }
+
+    async updateSchedule(scheduleId, data) {
+        const sql = 'UPDATE Schedules SET ? WHERE id=?;'
+        const values = [_.pickBy(data, _.identity), scheduleId] // _.pickBy ... _.identity removed null and undefined from object
+
+        try {
+            await database.query(sql, values)
+            let schedule = await this.getScheduleById(scheduleId)
+
+            if (!schedule) {
+                return Promise.reject({ code: 500, message: 'Unable to get Schedules after Update.' })
+            }
+
+            return schedule
+        } catch (err) {
+            console.log(err)
+            if (typeof err === 'object') {
+                return Promise.reject(err)
+            }
+
+            return Promise.reject({ code: 500, message: 'Unable to update Schedule.' })
+        }
+    }
+
+    async deleteSchedule(scheduleId) {
+        const sql = 'DELETE FROM Schedules WHERE id=?;'
+        const values = [scheduleId]
+
+        try {
+            await database.query(sql, values)
+        } catch (err) {
+            console.log(err)
+            return Promise.reject({ code: 500, message: 'Unable to delete Schedule.' })
+        }
+    }
+}
