b57c0135dd88b6d3f78a3a85b6f0bcdf16e80eee
Kendrick Parks <bordersmbk02@aol.com>
2021-03-14 15:08:07 -0700

    get creat and update user apis

--- app/src/contexts/UserContext.js
+++ app/src/contexts/UserContext.js
@@ -1,15 +1,14 @@
 import React, { createContext, useState, useEffect, useContext } from 'react'
 import { AuthContext } from './AuthContext'
 import axios from 'axios'
+import * as _ from 'lodash'
 
 export const UserContext = createContext()
 
 export const UserContextProvider = (props) => {
     const { getAccessToken } = useContext(AuthContext)
-    const [users, setUsers] = useState([])
 
-
-    const getUserByProgram = async (programId) => {
+    const getUsersByProgram = async (programId) => {
         let token = getAccessToken()
         let results = await axios.get(`/programs/${programId}/users`, {
             headers: {
@@ -19,15 +18,54 @@ export const UserContextProvider = (props) => {
             return Promise.reject(`Unable to Get Users. ${err}.`)
         })
 
-        if (results && results.data) {
-            setUsers(results.data.users || [])
-        }
+        return results.data.users
+    }
+
+    const getUsersByAccount = async () => {
+        let token = getAccessToken()
+        let results = await axios.get(`/users`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(`Unable to Get Users. ${err}.`)
+        })
 
         return results.data.users
     }
 
+    const createUser = async (data) => {
+        let token = getAccessToken()
+        let results = await axios.post(`/users`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(_.get(err, 'response.data.error') || 'Unable to create User')
+        })
+
+        results.data.user.type = results.data.roles[0].role
+        return results.data.user
+    }
+
+    const updateUser = async (data) => {
+        let token = getAccessToken()
+        let results = await axios.put(`/users/${data.id}`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(_.get(err, 'response.data.error') || 'Unable to update User')
+        })
+
+        return {
+            user: results.data.user,
+            roles: results.data.roles
+        }
+    }
+
     return (
-        <UserContext.Provider value={{ users, getUserByProgram }}>
+        <UserContext.Provider value={{ getUsersByProgram, getUsersByAccount, createUser, updateUser }}>
             {props.children}
         </UserContext.Provider>
     )
