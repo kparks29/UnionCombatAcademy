233c6268b43797c473f4daf094a13c5a80df13bd
Kendrick Parks <bordersmbk02@aol.com>
2021-03-14 16:31:14 -0700

    create and update news api

--- app/src/contexts/NewsContext.js
+++ app/src/contexts/NewsContext.js
@@ -1,13 +1,12 @@
 import React, { createContext, useState, useEffect, useContext } from 'react'
 import { AuthContext } from './AuthContext'
 import axios from 'axios'
+import * as _ from 'lodash'
 
 export const NewsContext = createContext()
 
 export const NewsContextProvider = (props) => {
     const { getAccessToken } = useContext(AuthContext)
-    const [news, setNews] = useState([])
-
 
     const getNews = async (programId) => {
         let token = getAccessToken()
@@ -19,15 +18,37 @@ export const NewsContextProvider = (props) => {
             return Promise.reject(`Unable to Get News. ${err}.`)
         })
 
-        if (results && results.data) {
-            setNews(results.data.news || [])
-        }
+        return results.data.news
+    }
+
+    const createNews = async (data) => {
+        let token = getAccessToken()
+        let results = await axios.post(`/news`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(_.get(err, 'response.data.error') || 'Unable to create News')
+        })
+
+        return results.data.news
+    }
+
+    const updateNews = async (data) => {
+        let token = getAccessToken()
+        let results = await axios.put(`/news/${data.id}`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(_.get(err, 'response.data.error') || 'Unable to update News')
+        })
 
         return results.data.news
     }
 
     return (
-        <NewsContext.Provider value={{ news, getNews }}>
+        <NewsContext.Provider value={{ getNews, createNews, updateNews }}>
             {props.children}
         </NewsContext.Provider>
     )
