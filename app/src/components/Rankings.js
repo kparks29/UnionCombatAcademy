f3c406e3883f704597b6bea19e293bb2ac782ee1
Kendrick Parks <bordersmbk02@aol.com>
2021-03-14 18:09:07 -0700

    be able to checkin user

--- app/src/components/Rankings.js
+++ app/src/components/Rankings.js
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react'
-import './News.css'
+import './Rankings.css'
 import { AuthContext } from '../contexts/AuthContext'
 import { UserContext } from '../contexts/UserContext'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 import * as _ from 'lodash'
+import moment from 'moment'
 
 export const Rankings = () => {
-    const { isAuthenticated, currentProgram } = useContext(AuthContext)
-    const { getUsersByProgram } = useContext(UserContext)
+    const { isAuthenticated, currentProgram, currentUser } = useContext(AuthContext)
+    const { getUsersByProgram, checkInUser } = useContext(UserContext)
     const [ users, setUsers ] = useState([])
     const [ belts, setBelts ] = useState({})
     const [ isLoading, setIsLoading] = useState(true)
+    const [isAdmin, setIsAdmin] = useState(false)
 
     const beltColors = [
         'white', 'yellow', 'orange', 'green', 'gray', 'blue', 
@@ -18,18 +20,25 @@ export const Rankings = () => {
     ]
 
     useEffect(() => {
-        if (isAuthenticated && currentProgram) {
+        if (isAuthenticated && currentProgram && currentUser) {
             getUsersByProgram(currentProgram.id).then(results => {
                 setIsLoading(false)
                 if (results) {
-                    setUsers(results.filter(user => user.type === 'student'))
+                    let users = results.filter(user => user.type === 'student')
+                    users.forEach(user => user.hasCheckedIn = hasCheckedIn(user))
+                    setUsers(users)
                 }
             }).catch(err => {
                 setIsLoading(false)
                 console.log(err)
             })
+
+            let role = currentUser.roles.find(role => role.programId === currentProgram.id)
+            if (role && ['admin', 'owner', 'instructor'].includes(role.role)) {
+                setIsAdmin(true)
+            }
         }
-    }, [isAuthenticated, currentProgram])
+    }, [isAuthenticated, currentProgram, currentUser])
 
     useEffect(async () => {
         let beltImages = {}
@@ -39,6 +48,36 @@ export const Rankings = () => {
         setBelts(beltImages)
     }, [])
 
+    const hasCheckedIn = (user) => {
+        let hasCheckedIn = false
+        user.attendance.forEach(attendance => {
+            if (moment(attendance.createdAt).isSame(new Date(), 'day')) {
+                hasCheckedIn = true
+            }
+        })
+        return hasCheckedIn
+    }
+
+    const onCheckIn = async (user) => {
+        try {
+            let attendance = await checkInUser({ programId: currentProgram.id, userId: user.id })
+            user.attendance.push(attendance)
+            user.hasCheckedIn = true
+
+            let students = [...users]
+            for (let i = 0; i < students.length; i++) {
+                if (user.id === students[i].id) {
+                    console.log('updating user', user)
+                    students[i] = user
+                }
+            }
+
+            setUsers(students)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div className="Rankings">
             <h2>Rankings</h2>
@@ -47,7 +86,7 @@ export const Rankings = () => {
                 users.map((user, i) => {
                     return <Card key={i}>
                         <Card.Body>
-                            <Card.Title>{user.firstName} {user.lastName}</Card.Title>
+                            <Card.Title>{user.firstName} {user.lastName} {isAdmin ? !user.hasCheckedIn ? <Button onClick={() => onCheckIn(user)}>Check In</Button> : <Button disabled>Already Checked In</Button> : ''}</Card.Title>
                             {!belts[user.beltColor] ? '' : <img src={belts[user.beltColor]} alt={user.beltColor + ' belt'} />}
                             <br />{user.stripeCount} Stripes
                         </Card.Body>
