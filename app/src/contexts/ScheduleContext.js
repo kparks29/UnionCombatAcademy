0517083f2d81ccb0d05da13b761d460db83b9c35
Kendrick Parks <bordersmbk02@aol.com>
2021-03-14 18:07:59 -0700

    add create and update apis

--- app/src/contexts/ScheduleContext.js
+++ app/src/contexts/ScheduleContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useContext } from 'react'
 import { AuthContext } from './AuthContext'
 import axios from 'axios'
+import * as _ from 'lodash'
 
 export const ScheduleContext = createContext()
 
@@ -20,8 +21,34 @@ export const ScheduleContextProvider = (props) => {
         return results.data.schedules
     }
 
+    const createSchedule = async (data) => {
+        let token = getAccessToken()
+        let results = await axios.post(`/schedules`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(_.get(err, 'response.data.error') || 'Unable to create schedule')
+        })
+
+        return results.data.schedule
+    }
+
+    const updateSchedule = async (data) => {
+        let token = getAccessToken()
+        let results = await axios.put(`/schedules/${data.id}`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).catch(err => {
+            return Promise.reject(_.get(err, 'response.data.error') || 'Unable to update Schedule')
+        })
+
+        return results.data.schedule
+    }
+
     return (
-        <ScheduleContext.Provider value={{ getSchedulesByProgramId }}>
+        <ScheduleContext.Provider value={{ getSchedulesByProgramId, createSchedule, updateSchedule }}>
             {props.children}
         </ScheduleContext.Provider>
     )
